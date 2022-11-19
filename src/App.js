1ef0f010adb0d74c81043d5a7a8c2e50a0c9bd4c
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-19 18:28:24 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Slides } from "./components/Slides";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import people from "./shared/data";
 
 function App() {
+  const [persons, setPeople] = useState(people);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex(index + 1);
+      if (index === persons.length) {
+        setIndex(0);
+      }
+    }, 6000);
+    return () => clearInterval(timer);
+  });
+
+  const nextPerson = () => {
+    if (index === persons.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  const prevPerson = () => {
+    if (index === 0) {
+      setIndex(persons.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
+
   return (
     <section className="section">
       <div className="title">
@@ -13,10 +43,10 @@ function App() {
       <div className="section-center">
         <Slides />
         <button className="prev">
-          <FiChevronLeft />
+          <FiChevronLeft prevPerson={prevPerson} />
         </button>
         <button className="next">
-          <FiChevronRight />
+          <FiChevronRight nextPerson={nextPerson} />
         </button>
       </div>
     </section>
