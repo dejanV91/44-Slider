c51dbbecee2f2d1fd5a708cb7c7f595324333da0
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-19 16:40:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-function App() {}
+import React from "react";
+import { Slides } from "./components/Slides";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+
+function App() {
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        <Slides />
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
 
 export default App;
---
+++ src/components/Slides.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaQuoteRight } from "react-icons/fa";
+
+export const Slides = () => {
+  return (
+    <article>
+      <img src="" className="person-img" alt="" />
+      <h4>maria ferguson</h4>
+      <p className="title">frontend</p>
+      <p className="text">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, obcaecati.
+      </p>
+      <FaQuoteRight className="icon" />
+    </article>
+  );
+};
